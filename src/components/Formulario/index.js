d5275fa676e073749b1acb2e88b3cbc0ad313eba
bzr-lipe <bzr.lipe@gmail.com>
2023-02-15 12:15:19 -0300

    form on progress...

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,21 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
+
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
+
     return (
         <section className="formulario">
             <form>
@@ -9,6 +23,7 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
             <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
             <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+            <ListaSuspensa itens={times} label="Time"/>
             </form>
         </section>
     )
