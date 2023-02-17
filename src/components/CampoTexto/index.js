48af123aa435d17666c7cec379ee0f7c0257484c
bzr-lipe <bzr.lipe@gmail.com>
2023-02-17 12:25:42 -0300

    LISTA OK

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,19 @@
 import './CampoTexto.css'
+import {useState} from 'react';
+
 
 const CampoTexto = (props) => {
+
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input value ={props.valor} onChange={aoDigitar} placeholder={props.placeholder} required={props.obrigatorio}/>
         </div>
     )
     
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -14,16 +16,60 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
+
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('Programação');
+
+    const aoSalvar = (event) => {
+        event.preventDefault();
+        props.aoCadastrar([
+            nome,
+            cargo,
+            imagem,
+            time
+        ]
+        );
+    }
     
 
     return (
         <section className="formulario">
-            <form>
-            <h2>Preencha os dados para criar o card do colaborador:</h2>
-            <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
-            <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
-            <ListaSuspensa itens={times} label="Time"/>
+            <form onSubmit = {aoSalvar}>
+            <h2>Preencha dados para criar o card do colaborador:</h2>
+            <CampoTexto 
+                obrigatorio="true" 
+                label="Nome" 
+                placeholder="Digite seu Nome"
+                valor={nome}
+                aoAlterado ={valor => setNome(valor)}
+            />
+            <CampoTexto 
+                obrigatorio="true" 
+                label="Cargo" 
+                placeholder="Digite seu Cargo"
+                valor={cargo}
+                aoAlterado ={valor => setCargo(valor)}
+            />
+            <CampoTexto 
+                obrigatorio="true" 
+                label="Imagem" 
+                placeholder="Informe o endereço da imagem"
+                valor={imagem}
+                aoAlterado ={valor => setImagem(valor)}
+            />
+            <ListaSuspensa 
+                obrigatorio="true"
+                label="Time"
+                itens={times} 
+                valor={time}
+                aoAlterado = {valor => setTime(valor)}
+            />
+            
+            <Botao >
+                Criar Card
+            </Botao>
             </form>
         </section>
     )
